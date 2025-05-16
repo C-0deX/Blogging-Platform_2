9ae0f52c75a817319b0ffc8d7bced5650b3ced65
Mahatab_Sahu <mahatabsahu071@gmail.com>
2025-05-16 23:42:21 +0530

    Fix: added https to CORS origin

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const connectDB=async()=>{
 app.use(express.json())
 // app.use(cors({origin:"http://localhost:5173",credentials:true}))
 
-app.use(cors({origin:"online-news-and-blogging-platform-1.vercel.app",credentials:true}))
+app.use(cors({origin:"https://online-news-and-blogging-platform-1.vercel.app",credentials:true}))
 
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
